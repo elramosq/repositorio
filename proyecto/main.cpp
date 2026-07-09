e7152fa568963c3bd59b44a7be348f027c16ba7a
elramosq <elramosq@unjbg.edu.pe>
2026-07-09 02:02:07 -0500

    Mejoras y cambios

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+#include "estructura.h"
+#include "wilson.h"
+#include "erick.h"
+#include "ericka.h"
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
+	
+	
 	
 	return 0;
 }
