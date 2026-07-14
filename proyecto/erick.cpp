6ef358020da2c05b6b2efbcaa207312a33536b33
eticonap <eticonap@unjbg.edu.pe>
2026-07-13 23:45:22 -0500

    buscarArtefacto: Implementando menu de opciones y verificación de catalogo vacio

--- proyecto/erick.cpp
+++ proyecto/erick.cpp
@@ -5,6 +5,14 @@
 using namespace std;
 
 void buscarArtefacto(Artefacto catalogo[], int cantidad){
+	if (cantidad==0){
+		cout<<"\nALERTA: El catalogo esta vacio. No hay productos disponibles."<<endl;
+		return;
+	}
+	
+	int opcionBusqueda;
+	cout<< "\n--- BUSCAR ARTEFACTO ---\n1. Por codigo\n2. Por marca\nOpcion: ";
+	cin >> opcionBusqueda;	
 }
 
 void descontinuarProducto(Artefacto catalogo[], int cantidad){
