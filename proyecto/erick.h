cbff16cc3f83c6cd79505577e4c5c42349e9ce8d
eticonap <eticonap@unjbg.edu.pe>
2026-07-13 20:40:47 -0500

    Mejora: directivas de proteccion agregadas

--- proyecto/erick.h
+++ proyecto/erick.h
@@ -1,5 +1,9 @@
+#ifndef ERICK_H
+#define ERICK_H
 #include <iostream>
 #include "estructura.h"
 
 void buscarArtefacto(Artefacto catalogo[], int cantidad);
 void descontinuarProducto(Artefacto catalogo[], int cantidad);
+
+#endif
