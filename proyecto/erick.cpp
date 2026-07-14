197220897db507a5f7234a6779875f3a28edaed6
eticonap <eticonap@unjbg.edu.pe>
2026-07-14 00:12:53 -0500

    buscarArtefacto: Implementando busqueda por codigo de producto

--- proyecto/erick.cpp
+++ proyecto/erick.cpp
@@ -12,7 +12,30 @@ void buscarArtefacto(Artefacto catalogo[], int cantidad){
 	
 	int opcionBusqueda;
 	cout<< "\n--- BUSCAR ARTEFACTO ---\n1. Por codigo\n2. Por marca\nOpcion: ";
-	cin >> opcionBusqueda;	
+	cin >> opcionBusqueda;
+	
+	bool encontrado=false;
+	
+	if (opcionBusqueda==1){
+		int codigoBuscado;
+		cout<<"Ingrese el codigo: ";
+		cin >> codigoBuscado;
+		
+		for (int i=0; i<cantidad; i++){
+			if(catalogo[i].codigo==codigoBuscado){
+				cout<< "\n--- Artefacto Encontrado ---" <<endl;
+				cout<< "Codigo: " << catalogo[i].codigo <<endl;
+				cout<< "Nombre: " << catalogo[i].nombre <<endl;
+				cout<< "Marca: " << catalogo[i].marca <<endl;
+				cout<< "Stock: " << catalogo[i].stock <<endl;
+				cout<< "Precio: S/. " << catalogo[i].precio <<endl;
+				cout<< "Estado: " << (catalogo[i].activo ? "Activo": "Descontinuado")<<endl;
+				encontrado=true;
+				break;
+			}
+		}
+		if (!encontrado) cout<<"\nAlerta: No se encontro el codigo."<<endl;
+	}
 }
 
 void descontinuarProducto(Artefacto catalogo[], int cantidad){
