6563b21bdb42161f8f43d516248f9eac9be108b1
eticonap <eticonap@unjbg.edu.pe>
2026-07-13 23:02:22 -0500

    Creando estructura base: librerias y funciones

---
+++ proyecto/erick.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "erick.h"
+
+using namespace std;
+
+void buscarArtefacto(Artefacto catalogo[], int cantidad){
+}
+
+void descontinuarProducto(Artefacto catalogo[], int cantidad){
+}
